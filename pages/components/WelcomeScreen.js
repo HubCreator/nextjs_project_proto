f466f8ffdfc3dbda333c76a482896c0469bb1ab5
HyunSeong Jeong <chocomilk8604@gmail.com>
2021-08-05 12:41:47 +0900

    Added videos

---
+++ pages/components/WelcomeScreen.js
@@ -0,0 +1,42 @@
+const WelcomeScreen = () => {
+  return (
+    <>
+      <section>
+        <div className="side" id="side1">
+          <video src="note.mp4" type="video/mp4" autoPlay loop></video>
+        </div>
+        <div className="side" id="side2">
+          <video src="note.mp4" type="video/mp4" autoPlay loop></video>
+        </div>
+      </section>
+      <div className="content">
+        <h2>Split Video on Page scroll</h2>
+        <p>
+          Improve app quality in less time with less effort. Simplify testing,
+          triaging, and troubleshooting. Carefully roll out features and monitor
+          adoption. Pinpoint, prioritize, and fix stability and performance
+          issues early. Boost user engagement with rich analytics, A/B testing,
+          and messaging campaigns. Understand your users to better support and
+          retain them. Run experiments to test ideas and uncover new insights.
+          Customize your app for different user segments. Improve app quality in
+          less time with less effort. Simplify testing, triaging, and
+          troubleshooting. Carefully roll out features and monitor adoption.
+          Pinpoint, prioritize, and fix stability and performance issues early.
+          Boost user engagement with rich analytics, A/B testing, and messaging
+          campaigns. Understand your users to better support and retain them.
+          Run experiments to test ideas and uncover new insights. Customize your
+          app for different user segments. Improve app quality in less time with
+          less effort. Simplify testing, triaging, and troubleshooting.
+          Carefully roll out features and monitor adoption. Pinpoint,
+          prioritize, and fix stability and performance issues early. Boost user
+          engagement with rich analytics, A/B testing, and messaging campaigns.
+          Understand your users to better support and retain them. Run
+          experiments to test ideas and uncover new insights. Customize your app
+          for different user segments.
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default WelcomeScreen;
