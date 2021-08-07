d9904595c59be1481b770af507337161fdab03cb
HyunSeong Jeong <chocomilk8604@gmail.com>
2021-08-07 16:06:07 +0900

    Added screen media query for mobile display

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,29 @@
+import { IoLogoIonitron } from "react-icons/io";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { AiOutlineClose } from "react-icons/ai";
+import { useState } from "react";
+
 const Navbar = () => {
+  const [navStatus, setNavStatus] = useState(false);
   return (
     <>
       <section className="navbar-container">
-        <div className="navbar-container__logo">
-          <div className="navbar-container__logo__img"></div>
-          <div className="navbar-container__logo__name"></div>
-        </div>
-        <div className="navbar-container__links">
-          <div className="links__link">Home</div>
-          <div className="links__link">Explore</div>
-          <div className="links__link">Settings</div>
+        <div className="navbar-container__contents">
+          <div className="contents__logo">
+            <div className="contents__logo__img">
+              <IoLogoIonitron />
+            </div>
+            <div className="contents__logo__name">Welcome</div>
+          </div>
+
+          <div className="contents__controller">
+            {navStatus ? <GiHamburgerMenu /> : <AiOutlineClose />}
+          </div>
+          <div className="contents__links">
+            <div className="links__alink">Home</div>
+            <div className="links__alink">Explore</div>
+            <div className="links__alink">Settings</div>
+          </div>
         </div>
       </section>
     </>
