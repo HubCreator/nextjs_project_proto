455936d61d43621ab6ec8ea5c6851b4275288d5c
HyunSeong Jeong <chocomilk8604@gmail.com>
2021-08-07 16:56:15 +0900

    All I wanted was just toglling when it's clicked.. but couldn't make it. What's wrong with it?

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,29 +4,35 @@ import { AiOutlineClose } from "react-icons/ai";
 import { useState } from "react";
 
 const Navbar = () => {
-  const [navStatus, setNavStatus] = useState(false);
-  return (
-    <>
-      <section className="navbar-container">
-        <div className="navbar-container__contents">
-          <div className="contents__logo">
-            <div className="contents__logo__img">
-              <IoLogoIonitron />
-            </div>
-            <div className="contents__logo__name">Welcome</div>
-          </div>
+  const [navStatus, setNavStatus] = useState(true);
+  const handleClick = () => {
+    // console.log(e);
+    setNavStatus(!navStatus);
+  };
 
-          <div className="contents__controller">
-            {navStatus ? <GiHamburgerMenu /> : <AiOutlineClose />}
+  return (
+    <section className="navbar-container">
+      <div className="navbar-container__contents">
+        <div className="contents__logo">
+          <div className="contents__logo__img">
+            <IoLogoIonitron />
           </div>
-          <div className="contents__links">
-            <div className="links__alink">Home</div>
-            <div className="links__alink">Explore</div>
-            <div className="links__alink">Settings</div>
-          </div>
+          <div className="contents__logo__name">Welcome</div>
         </div>
-      </section>
-    </>
+
+        <div className="contents__controller" onClick={handleClick}>
+          {navStatus ? <GiHamburgerMenu /> : <AiOutlineClose />}
+        </div>
+
+        <ul
+          className={navStatus ? "contents__links active" : "contents__links"}
+        >
+          <li className="links__alink">Home</li>
+          <li className="links__alink">Explore</li>
+          <li className="links__alink">Settings</li>
+        </ul>
+      </div>
+    </section>
   );
 };
 
