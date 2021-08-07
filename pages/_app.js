1c8d39999269c6e6d4f7af1cf9337424ed583981
HyunSeong Jeong <chocomilk8604@gmail.com>
2021-08-07 23:53:05 +0900

    You shouldn't use dot on your scss file name

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import "../styles/globals.css";
-import "../styles/Navbar.module.scss";
+import "../styles/Navbar.scss";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
