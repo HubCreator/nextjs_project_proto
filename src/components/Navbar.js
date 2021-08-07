d0b5286be2d245ab76083eacecf1b615cc484a66
HyunSeong Jeong <chocomilk8604@gmail.com>
2021-08-07 17:26:48 +0900

    You can't handle click or something in section tag. Be aware... Make sure you have div tag

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,33 +6,46 @@ import { useState } from "react";
 const Navbar = () => {
   const [navStatus, setNavStatus] = useState(true);
   const handleClick = () => {
-    // console.log(e);
+    console.log("hi");
     setNavStatus(!navStatus);
   };
 
   return (
-    <section className="navbar-container">
-      <div className="navbar-container__contents">
-        <div className="contents__logo">
-          <div className="contents__logo__img">
-            <IoLogoIonitron />
+    <>
+      <div className="navbar-container">
+        <div className="navbar-container__contents">
+          <div className="contents__logo">
+            <div className="contents__logo__img">
+              <IoLogoIonitron />
+            </div>
+            <div className="contents__logo__name">Welcome</div>
           </div>
-          <div className="contents__logo__name">Welcome</div>
-        </div>
 
-        <div className="contents__controller" onClick={handleClick}>
-          {navStatus ? <GiHamburgerMenu /> : <AiOutlineClose />}
-        </div>
+          <div className="contents__controller" onClick={handleClick}>
+            {navStatus ? <GiHamburgerMenu /> : <AiOutlineClose />}
+          </div>
 
-        <ul
-          className={navStatus ? "contents__links active" : "contents__links"}
-        >
-          <li className="links__alink">Home</li>
-          <li className="links__alink">Explore</li>
-          <li className="links__alink">Settings</li>
-        </ul>
+          <ul
+            className={navStatus ? "contents__links active" : "contents__links"}
+          >
+            <li className="links__alink">Home</li>
+            <li className="links__alink">Explore</li>
+            <li className="links__alink">Settings</li>
+          </ul>
+        </div>
       </div>
-    </section>
+      {/* <button className="button" onClick={() => console.log("hello")}>
+        hello
+      </button> */}
+      <div
+        className="button"
+        onClick={() => {
+          console.log("fuck");
+        }}
+      >
+        hello
+      </div>
+    </>
   );
 };
 
