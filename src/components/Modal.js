a323f3bd2bc2f704dea9c95f5de5fd4b0255cf1d
HyunSeong Jeong <chocomilk8604@gmail.com>
2021-08-08 13:48:55 +0900

    Making a form. Having a problem with height and line of textarea

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -10,16 +10,16 @@ const Modal = () => {
     <div className="modal-screen">
       <div className="modal-container">
         <div className="container__blueBg">
-          <div className="container__blueBg__box signIn">
+          <div className="container__blueBg__box leftBox">
             <h2 className="box__msg">Already Have an Account ?</h2>
-            <button className="box__btn signIn" onClick={onClick}>
+            <button className="box__btn" onClick={onClick}>
               Sign in
             </button>
           </div>
 
-          <div className="container__blueBg__box signUp">
+          <div className="container__blueBg__box rightBox">
             <h2 className="box__msg">Don't Have an Account ?</h2>
-            <button className="box__btn signUp" onClick={onClick}>
+            <button className="box__btn" onClick={onClick}>
               Sign up
             </button>
           </div>
@@ -27,11 +27,28 @@ const Modal = () => {
         <div
           className={
             signStatus
-              ? "container__formBox signInForm"
-              : "container__formBox signUpForm"
+              ? "container__formBox leftBox"
+              : "container__formBox rightBox"
           }
         >
-          hi
+          {signStatus ? (
+            <>
+              <div className="formBox__content formBox__title">
+                <span>Title</span>
+                <input type="text" />
+              </div>
+              <div className="formBox__content formBox__description">
+                <span>Description</span>
+                <input type="text" />
+              </div>
+              <div className="formBox__content formBox__name">
+                <span>Name</span>
+                <input type="text" />
+              </div>
+            </>
+          ) : (
+            <div>hello</div>
+          )}
         </div>
       </div>
     </div>
