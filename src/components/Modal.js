305e58737c876194dfa1e3e2789743cec390c7aa
HyunSeong Jeong <chocomilk8604@gmail.com>
2021-08-08 14:06:41 +0900

    I got typo about textarea. Done with it and made a useState for input title, desc and name

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -2,8 +2,25 @@ import { useState } from "react";
 
 const Modal = () => {
   const [signStatus, setSignStatus] = useState(true); // true: left
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [name, setName] = useState("");
   const onClick = () => {
     setSignStatus(!signStatus);
+  };
+
+  const onChange = (event) => {
+    const {
+      target: { value, className },
+    } = event;
+
+    if (className === "formBox__content__title") {
+      setTitle(value);
+    } else if (className === "formBox__content__description") {
+      setDescription(value);
+    } else {
+      setName(value);
+    }
   };
 
   return (
@@ -11,9 +28,9 @@ const Modal = () => {
       <div className="modal-container">
         <div className="container__blueBg">
           <div className="container__blueBg__box leftBox">
-            <h2 className="box__msg">Already Have an Account ?</h2>
+            <h2 className="box__msg">Sure to post public?</h2>
             <button className="box__btn" onClick={onClick}>
-              Sign in
+              Go back
             </button>
           </div>
 
@@ -35,15 +52,33 @@ const Modal = () => {
             <>
               <div className="formBox__content formBox__title">
                 <span>Title</span>
-                <input type="text" />
+                <input
+                  className="formBox__content__title"
+                  type="text"
+                  placeholder="Title.."
+                  value={title}
+                  onChange={onChange}
+                />
               </div>
               <div className="formBox__content formBox__description">
                 <span>Description</span>
-                <input type="text" />
+                <textarea
+                  className="formBox__content__description"
+                  type="text"
+                  placeholder="Description.."
+                  value={description}
+                  onChange={onChange}
+                />
               </div>
               <div className="formBox__content formBox__name">
                 <span>Name</span>
-                <input type="text" />
+                <input
+                  className="formBox__content__name"
+                  type="text"
+                  placeholder="Name.."
+                  value={name}
+                  onChange={onChange}
+                />
               </div>
             </>
           ) : (
